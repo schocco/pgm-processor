c8b6170cc2c78119bd269fa878036ae465d5f8fb
Rocco Schulz <schocco.rulz@googlemail.com>
2013-06-15 16:00:43 +0200

    cleaned up.

--- com.is-gr8.image-processing/src/main/java/com/is_gr8/imageprocessor/ui/PreviewComposite.java
+++ com.is-gr8.image-processing/src/main/java/com/is_gr8/imageprocessor/ui/PreviewComposite.java
@@ -50,22 +50,10 @@ public class PreviewComposite extends Composite implements ImageEventListener{
 		//create image data from pgm object
 		int depth = 8; //TODO: this should be stored in the pgm object
 		PaletteData palette = new PaletteData(255, 255, 255);
-		idata = new ImageData(pgm.getWidth(), pgm.getHeight(), depth, palette);
-		
-		//read image data from source file if present
-		//idata = new ImageData(pgm.getFile().getAbsolutePath());
-		
 		byte[][] px = pgm.getPixels();
 		byte[] imagedata = new byte[pgm.getWidth() * pgm.getHeight() * 32/depth];
 		
 		int bytecounter = 0;
-		for(int row = 0; row < pgm.getHeight(); row++){
-			for(int col = 0; col < pgm.getWidth(); col++){
-				idata.data[bytecounter++] = px[row][col];
-			}
-		}
-		
-		bytecounter = 0;
 		for(int row = 0; row < pgm.getHeight(); row++){
 			for(int col = 0; col < pgm.getWidth(); col++){
 				imagedata[bytecounter++] = px[row][col];
@@ -73,27 +61,9 @@ public class PreviewComposite extends Composite implements ImageEventListener{
 		}
 		
 		idata = new ImageData(pgm.getWidth(), pgm.getHeight(), depth, palette, 1, imagedata);
-		
+
 		//create and display image
 		Image img = new Image(display, idata);
-		//FIXME: only works for landscape oriented images for some reason.
-		//possible workaround: create temporary file and create swt image from inputstream
-		logger.debug(bytecounter + " vs " + img.getImageData().data.length);
-		logger.debug("bytes per line: " + img.getImageData().bytesPerLine + " width: " + pgm.getWidth());
-		
-//		for (int y = 0, j = 3, n = 0; y < 50; y++) {
-//			for (int x = 0; x < pgm.getWidth(); x++) {
-//				byte orig = px[y][x];
-//				byte nev = (img.getImageData().data[j]);
-//				byte seq = img.getImageData().data[n++];
-//				if (x % 20 == 0 && nev != orig) {
-//					logger.debug(orig + " vs " + nev + " vs " + seq);
-//				}
-//
-//				j += 4;
-//			}
-//		}
-		
 		imgLabel.setImage(img);
 		this.layout();
 	}
